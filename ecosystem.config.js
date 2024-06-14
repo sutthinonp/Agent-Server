545cbe6d84c8b3b11e15dc1ba52be8beb0afdfb2
sutthinonp <138119061+sutthinonp@users.noreply.github.com>
2024-06-14 18:01:12 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,21 +9,9 @@ module.exports = {
         max_memory_restart: '1G',
         env: {
           NODE_ENV: 'development',
-          PORT: 3001,
-          DATABASE_HOST: '128.199.184.27',
-          DATABASE_PORT: 3306,
-          DATABASE_NAME: 'crm',
-          DATABASE_USER: 'readuser',
-          DATABASE_PASSWORD: '12003000'
         },
         env_production: {
           NODE_ENV: 'production',
-          PORT: 3001,
-          DATABASE_HOST: '128.199.184.27',
-          DATABASE_PORT: 3306,
-          DATABASE_NAME: 'crm',
-          DATABASE_USER: 'readuser',
-          DATABASE_PASSWORD: '12003000'
         }
       }
     ]
